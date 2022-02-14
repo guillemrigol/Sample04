5583bd2ab480737702b73000bf88abcdec937dc4
tricacole <majidassikiou@gmail.com>
2022-02-14 22:58:57 +0100

    Majid Assikiou 14/02/2022 22:58

--- collabs/majid.php
+++ collabs/majid.php
@@ -1,1 +1,23 @@
-<li><b> Majid Assikiou </b></li>
+<li>
+
+<style>
+
+  div.test
+  {
+    white-space: nowrap;
+    width: 55px;
+    overflow: hidden;
+    color: blue;
+  }
+
+  div.test:hover
+  {
+    overflow: visible;
+    width: 100px;
+  }
+
+</style>
+
+  <div class="test" style="text-overflow:ellipsis;"> Majid Assikiou </div>
+
+</li>
