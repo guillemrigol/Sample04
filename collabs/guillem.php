b6a7ac2cda3e4e4ef916b51d931abb265d71c748
Guillem Rigol <grigol@xtec.cat>
2022-02-14 20:59:50 +0100

    GR 14/02 20:59

---
+++ collabs/guillem.php
@@ -0,0 +1,1 @@
+<p><b>Prof.:  Guillem</b></p>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ca" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Directori Sample04</title>
+  </head>
+  <body>
+    <?php include('collabs/guillem.php') ?>
+    <?php include('collabs/albert.php') ?>
+  </body>
+</html>
