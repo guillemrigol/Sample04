b8cf5c8ad6960665a47299769381816c49e467f1
Guillem Trujillo <gtrujillocodina@gmail.com>
2022-02-14 21:16:49 +0100

    hola

---
+++ collabs/gTrujillo.php
@@ -0,0 +1,163 @@
+<p><b>Guillem Trujillo</b></p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style media="screen">
+
+@keyframes batec{
+  0%{
+    color: #FF0000
+  }
+  25%{
+    color: #F4FF00
+  }
+  50%{
+    color: #007EFF
+  }
+  75%{
+  color: #FF0000
+  }
+  100%{
+  color:#F4FF00
+  }
+}
+
+  p:hover{
+    transition: color ease 1s;
+  }
+</style>
