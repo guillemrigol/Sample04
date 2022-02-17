894e7af72516765046d77d760462f4f7ddb82372
matapa23 <bofill17@gmail.com>
2022-02-17 17:45:05 +0100

    Hola Guillem!!

---
+++ collabs/Marçal.php
@@ -0,0 +1,23 @@
+<li>
+
+<style>
+
+  div.test
+  {
+    white-space: nowrap;
+    width: 55px;
+    overflow: hidden;
+    color: blue;
+  }
+
+  div.test:hover
+  {
+    overflow: visible;
+    width: 100px;
+  }
+
+</style>
+
+  <div class="test" style="text-overflow:ellipsis;"> Marçal </div>
+
+</li>
